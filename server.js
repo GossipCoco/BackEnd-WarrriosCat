ead53fdab10bd9d1d52f488854b1479bd4645712
Coralie Honorin <gossipcoco@hotmail.fr>
2024-10-20 12:40:59 +0200

    fix big login

--- server.js
+++ server.js
@@ -15,7 +15,20 @@ require('dotenv').config();
 
 const Home = require('./app/Routes/Home')
 const User = require('./app/Routes/User')
-
+const Character = require('./app/routes/character')
+const Clan = require('./app/routes/clan')
+const Grade = require('./app/routes/grade')
+const Image = require('./app/routes/image')
+const Game = require('./app/routes/Game')
+const Fiction = require('./app/routes/fiction')
+const chatgptRouter = require('./app/routes/chatgpt');
+const Event = require('./app/routes/event')
+const Quest = require('./app/routes/quest')
+const imagegenRouter  = require('./app/routes/imagegen');
+const ArcBook = require('./app/routes/ArcBook')
+const Rating = require('./app/routes/Rating')
+const Chapter = require('./app/routes/Chapter')
+const Message = require('./app/routes/Message')
 const corsOptions = {
     // origin: '*',
     origin: [
@@ -29,18 +42,33 @@ const corsOptions = {
     }
   }
 app
-    .use('*',cors(corsOptions))
-    .use(bodyParser.urlencoded({ limit: '100mb', extended: true }))
-    .use(express.json())
-    .use(bodyParser.json({ limit: '100mb', extended: true }))
-    .use(express.urlencoded({ extended: true }))
-    .use(helmet())
-    .use('/Home', Home)
-    .use('/User', User)
-    .use(function (req, res) {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(404).send({ result: 'error' });
-      })
+.use('*',cors(corsOptions))
+  .use(bodyParser.urlencoded({ limit: '100mb', extended: true }))
+  .use(express.json())
+  .use(bodyParser.json({ limit: '100mb', extended: true }))
+  .use(express.urlencoded({ extended: true }))
+  .use(helmet())
+  // .use(limiter)
+  .use('/Home', Home)
+  .use('/quest', Quest)
+  .use('/character', Character)
+  .use('/clan', Clan)
+  .use('/grade', Grade)
+  .use('/image', Image)
+  .use('/game', Game)
+  .use('/fiction', Fiction)
+  .use('/api', chatgptRouter)
+  .use('/api', imagegenRouter)
+  .use('/event', Event)
+  .use('/user', User)
+  .use('/ArcBook', ArcBook)
+  .use('/chapter', Chapter)
+  .use('/rating', Rating)
+  .use('/message', Message)
+  .use(function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(404).send({ result: 'error' });
+  })
 
 const serv = http.createServer( app);
     serv.listen(PORT, (err) => {

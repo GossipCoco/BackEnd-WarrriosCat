a675d99ff382c28dd0e24d49608bb397e8a5cb03
Coralie Honorin <gossipcoco@hotmail.fr>
2025-05-12 20:32:13 +0200

    fix request refracto count query

--- app/Queries/CharacterQueries.js
+++ app/Queries/CharacterQueries.js
@@ -4,71 +4,6 @@ require('../Models/associations');
 const functions = require('../Functions/countFunctions')
 const { Op } = require("sequelize");
 
-
-const countAllCharacters = () => {
-    console.log("**** countAllCharacters   *****************");
-    const request = model.Character.findAndCountAll({ attributes: ['Id'] });
-    const promises = []
-    promises.push(request)
-    return functions.countFuntion(request)
-  };
-const CountCharacterByNameSearch = (id) => {
-  console.log("**** CountCharacterByNameSearch ****", id);
-  const request = model.Character.findAndCountAll({
-    where: {
-      Id: { [model.Utils.Op.like]: `%${id}%` },
-      CurrentName: { [model.Utils.Op.like]: `%${id}%` }
-    },
-    attributes: ['Id']
-  });
-  const promises = []
-  promises.push(request)
-  return functions.countFuntion(request)
-}
-const CountNbOriginaleCharacterByUser = (usr) => {  
-    console.log("**** CountNbOriginaleCharacterByUser   *****************", usr);
-    const request = model.Gamer.findAndCountAll({
-      where: { UserId: usr },
-      attributes: ['Id']
-    });
-    const promises = []
-    promises.push(request)    
-    return functions.countFuntion(request)  
-}
-const CountNbOriginaleCharacterByName = (id) => {
-  console.log("********** CountNbOriginaleCharacterByName *****************", id);
-
-  return model.Gamer.findAndCountAll({
-    where: {
-      CurrentName: {
-        [Op.like]: `%${id}%`
-      }
-    }
-  }).then(result => {
-    return { ob: { count: result.count } };
-  }).catch(err => {
-    console.error(err);
-    return { ob: { count: 0 }, error: true };
-  });
-};
-const CountNbCharactersByClan = (id) => {
-  console.log("**** CountNbCharactersByClan   *****************", id);
-  const request = model.Character.findAndCountAll({
-    where : { ClanId : id}
-  })
-  const promises = []
-  promises.push(request)
-  return functions.countFuntion(request)
-}
-const CountCharacterByGrade = id => {
-  console.log("**** CountNbCharactersByGrade   *****************", id);
-  const request = model.Character.findAndCountAll({
-    where : { GradeId : id}
-  })
-  const promises = []
-  promises.push(request)
-  return functions.countFuntion(request)
-}
 const GetAllCharacters = (nav) => {
     console.log("************ GetAllCharacters ************", nav)
     return model.Character.findAll({
@@ -149,8 +84,7 @@ const GetAllCharacters = (nav) => {
         { model: model.Grade },
         {
           model: model.Clan,
-          include: [{ model: model.Location }],
-          
+          include: [{ model: model.Location }],          
         },
         {
           model: model.Warrior,
@@ -414,12 +348,6 @@ const ReturnPromise = (promise, request) => {
   })
 }
   module.exports = {
-    countAllCharacters,
-    CountNbOriginaleCharacterByName,
-    CountCharacterByNameSearch,
-    CountNbOriginaleCharacterByUser,
-    CountNbCharactersByClan,
-    CountCharacterByGrade,
     GetAllCharacters,
     GetAllCharactersByClan,
     GetAllCharactersByGrade,

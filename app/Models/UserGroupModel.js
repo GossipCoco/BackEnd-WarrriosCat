797d9a8eebe67cb608c7a2ed5d37c1b4d09aefb4
Coralie Honorin <gossipcoco@hotmail.fr>
2025-05-30 10:50:29 +0200

    fix refracto fix group model usergroup

---
+++ app/Models/UserGroupModel.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+const connection = require('../DataLayer/connection');
+
+const  UserGroup = connection.define(
+    "UserGroup",
+    {
+      Id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+      },
+      Role:{
+        type: DataTypes.STRING,
+      },
+      CreatedAt:{
+        type: 'DATETIME',
+        defaultValue: new Date(functions.toDateTime(Date.now())).toISOString()
+      },
+      UpdatedAt:{
+        type: 'DATETIME',
+        defaultValue: new Date(functions.toDateTime(Date.now())).toISOString()
+      },
+
+    },
+    { freezeTableName: true, timestamps: false }
+)
+
+module.exports = UserGroup;

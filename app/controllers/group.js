ba53e2675948e711263f57e7523b64a6d7a9f029
Coralie Honorin <gossipcoco@hotmail.fr>
2025-05-30 11:40:19 +0200

    fix refracto fix group model usergroup route query controller

--- app/controllers/group.js
+++ app/controllers/group.js
@@ -5,5 +5,8 @@ const query = require('../Queries/GroupQueries')
 Group.GetAllGroups = (req, res) => {
     handleResponse(res, query.GetAllGroups())
 }
+Group.GetAllGroupsWithAllUsers = (req, res) => {
+    handleResponse(res, query.GetAllGroupsWithAllUsers())
+}
 
 module.exports = Group

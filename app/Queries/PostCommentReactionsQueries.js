07a3f7feec0b0fe304adb068b71e57e7c32951ce
Coralie Honorin <gossipcoco@hotmail.fr>
2025-10-23 17:38:17 +0200

    add reaction query fix

--- app/Queries/PostCommentReactionsQueries.js
+++ app/Queries/PostCommentReactionsQueries.js
@@ -9,11 +9,24 @@ const GetPostAllCommentReactions = (id) =>{
         limit: 3,
         include:[
             { model: model.User,attributes: ['UserName']},
-            {model: model.GroupComment},
-            {model: model.PostReaction}
+            { model: model.GroupComment},
+            { model: model.PostReaction}
         ]
     })
 }
+const CreateANewPost = (id, data) => {
+    console.log("**** CreateANewPost ****",id, data);
+    const date = new Date().toISOString();
+    const postId = uuidv4();
+    return model.GroupPost.create({
+        Id: postId,
+        Content: data.Content,
+        DateCreation: date,
+        UserId: data.UserId,
+        GroupId: data.id
+    })
+}
 module.exports = {
-    GetPostAllCommentReactions
+    GetPostAllCommentReactions,
+    CreateANewPost
 }
